a10310a88daf506d1b7c622a33153de20081bf75
Diego Costa <diegocosta@adm.unip.br>
2023-04-23 13:15:38 -0300

    parte da estrutura do card

--- js/api.js
+++ js/api.js
@@ -1,9 +1,11 @@
+let baseURL = "http://ergast.com/api/f1/drivers.json"
+
 var requestOptions = {
     method: 'GET',
     redirect: 'follow'
   };
   
-  fetch("http://ergast.com/api/f1/drivers.json", requestOptions)
+  fetch(baseURL, requestOptions)
     .then(response => response.json())
     .then(result => console.log(result))
     .catch(error => console.log('error', error));
