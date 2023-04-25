4eb268c022d9a4fc7a3c9c2300fe44201de6b107
Diego Costa <diegocosta@unip.br>
2023-04-25 17:08:08 -0300

    card consumindo as infos de um piloto

--- js/api.js
+++ js/api.js
@@ -1,11 +1,21 @@
-let baseURL = "http://ergast.com/api/f1/2022/drivers/.json"
+let baseURL = "http://ergast.com/api/f1/2022/drivers.json";
+const nome = document.getElementById("nome");
+const birth = document.getElementById("niver");
+const nacio = document.getElementById("nac");
+const number = document.getElementById("num");
 
-var requestOptions = {
+let requestOptions = {
     method: 'GET',
     redirect: 'follow'
   };
   
   fetch(baseURL, requestOptions)
     .then(response => response.json())
-    .then(result => console.log(result))
+    .then(result => {
+      console.log(result);
+      nome.innerHTML = result.MRData.DriverTable.Drivers[8].familyName;
+      birth.innerHTML = "Date of Birth: " + result.MRData.DriverTable.Drivers[8].dateOfBirth;
+      nacio.innerHTML = "Nacionality: " + result.MRData.DriverTable.Drivers[8].nationality;
+      number.innerHTML = "Pilot Number: " + result.MRData.DriverTable.Drivers[8].permanentNumber;
+    })
     .catch(error => console.log('error', error));
