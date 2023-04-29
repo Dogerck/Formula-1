77a690d63c55b59d45dc8dc710006a72b078780d
Diego Costa <diegocosta@unip.br>
2023-04-29 00:17:04 -0300

    aparecendo info de todos os pilotos nos cards

--- js/api.js
+++ js/api.js
@@ -1,22 +1,72 @@
 let baseURL = "http://ergast.com/api/f1/2023/drivers/.json";
-const nome = document.getElementById("nome");
-const birth = document.getElementById("niver");
-const nacio = document.getElementById("nac");
-const number = document.getElementById("num");
+const cardContent = document.querySelector(".container-cards");
 
+// const nome = document.getElementById("nome");
+// const birth = document.getElementById("niver");
+// const nacio = document.getElementById("nac");
+// const number = document.getElementById("num");
 let requestOptions = {
-    method: 'GET',
-    redirect: 'follow'
-  };
+  method: 'GET',
+  redirect: 'follow'
+};
+
+async function getCards() {
+  const response = await fetch(baseURL, requestOptions);
+  const data = await response.json()
+  
+    boys = data.MRData.DriverTable.Drivers;
+    console.log(boys);
+    console.log(response);
   
   
-    fetch(baseURL, requestOptions)
-    .then(response => response.json())
-    .then(result => {
-      result = result.MRData.DriverTable.Drivers;
-      console.log(result);
-    })
-    .catch(error => console.log('error', error));
+
+    boys.map((drivers) => {
+
+    const card = document.createElement("div");
+    card.classList.add("card");
+
+    const imgCard = document.createElement("div");
+    imgCard.classList.add("img-card");
+
+    const img = document.createElement("img");
+    img.setAttribute("src", `img/lec.png`);
+
+    const txtCtt = document.createElement("div");
+    txtCtt.classList.add("text-content");
+
+    const title = document.createElement("h3");
+    const linkwiki = document.createElement("a");
+    linkwiki.setAttribute("href", `${drivers.url}`);
+    linkwiki.setAttribute("target", `_blank`);
+    const niver = document.createElement("p");
+    const nac = document.createElement("p");
+    const num = document.createElement("p");
+    
+
+    cardContent.appendChild(card);
+    card.appendChild(imgCard);  
+    imgCard.appendChild(img);
+    card.appendChild(txtCtt);
+    txtCtt.appendChild(title);
+    title.appendChild(linkwiki);
+    txtCtt.appendChild(niver);
+    txtCtt.appendChild(nac);
+    txtCtt.appendChild(num);
+
+
+    linkwiki.innerText = drivers.givenName + " " + drivers.familyName;
+    niver.innerHTML = "<b>Birthday:</b> " + drivers.dateOfBirth;
+    nac.innerHTML = "<b>Nationality:</b> " + drivers.nationality;
+    num.innerHTML = "<b>Car Number:</b> " + drivers.permanentNumber
+
+
+  })
+}
+getCards()
+
+  
+  
+    
   
     
     
