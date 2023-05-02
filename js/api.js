89392a8ffbed66df9101e755d316dc9b7ffe9a60
Diego Costa <diegocosta@unip.br>
2023-05-01 21:12:48 -0300

    ultimos detalhes do card

--- js/api.js
+++ js/api.js
@@ -1,75 +1,87 @@
-let baseURL = "http://ergast.com/api/f1/2023/drivers.json";
+let baseURL = "http://ergast.com/api/f1";
 
+const drivers2023 = baseURL + '/2023/drivers.json';
+const standings = baseURL + '/current/driverStandings.json';
+console.log(standings);
 let requestOptions = {
   method: 'GET',
   redirect: 'follow'
 };
 
 async function getCards() {
-  await fetch(baseURL, requestOptions)
+
+  await fetch(drivers2023, requestOptions)
     .then(response => response.json())
     .then(data => {
 
       const cardContent = document.querySelector(".container-cards");
       data = data.MRData.DriverTable.Drivers;
-  
+
       data.map((drivers) => {
+
+        let birthDate = drivers.dateOfBirth.split('-').reverse().join('/')
+        drivers.dateOfBirth = birthDate
 
         const card = document.createElement("div");
         card.classList.add("card");
         card.setAttribute("id", drivers.driverId);
-    
+
         const imgCard = document.createElement("div");
         imgCard.classList.add("img-card");
-    
+
         const img = document.createElement("img");
         img.setAttribute("src", `assets/drivers/${drivers.driverId}.png`);
-    
+        img.setAttribute("alt", drivers.givenName);
+
+        const numPilot = document.createElement("img");
+        numPilot.setAttribute("src", `assets/pilot number/${drivers.driverId}.png`);
+        numPilot.classList.add("pilot-number");
+
         const txtCtt = document.createElement("div");
         txtCtt.classList.add("text-content");
-    
-        const title = document.createElement("h3");
+
+        const firstName = document.createElement("h3");
+        firstName.classList.add("firstName");
+        const secondName = document.createElement("h3");
+        secondName.classList.add("secondName");
         const linkwiki = document.createElement("a");
         linkwiki.setAttribute("href", drivers.url);
         linkwiki.setAttribute("target", `_blank`);
-        const niver = document.createElement("p");
+        const birth = document.createElement("p");
         const nac = document.createElement("p");
-        const num = document.createElement("p");
-        
-    
+
+
         cardContent.appendChild(card);
-        card.appendChild(imgCard);  
+        card.appendChild(imgCard);
+        imgCard.appendChild(numPilot);
         imgCard.appendChild(img);
         card.appendChild(txtCtt);
-        txtCtt.appendChild(title);
-        title.appendChild(linkwiki);
-        txtCtt.appendChild(niver);
+        imgCard.appendChild(firstName);
+        imgCard.appendChild(secondName);
+        secondName.appendChild(linkwiki);
+        txtCtt.appendChild(birth);
         txtCtt.appendChild(nac);
-        txtCtt.appendChild(num);
-    
-    
-        linkwiki.innerText = drivers.givenName + " " + drivers.familyName;
-        niver.innerHTML = "<b>Birthday:</b> " + drivers.dateOfBirth;
+
+
+        firstName.innerText = drivers.givenName;
+        linkwiki.innerText = drivers.familyName;
+        birth.innerHTML = "<b>Birthday:</b> " + drivers.dateOfBirth;
         nac.innerHTML = "<b>Nationality:</b> " + drivers.nationality;
-        num.innerHTML = "<b>Car Number:</b> " + drivers.permanentNumber;
       })
 
     })
     .catch(error => console.log('error', error))
-
-    let img = 
-    console.log(img);
 }
 getCards();
 
-  
-  
-    
-  
-    
-    
-    
-    
+
+
+
+
+
+
+
+
     // nome.innerHTML = result.MRData.DriverTable.Drivers[8].givenName + " " + result.MRData.DriverTable.Drivers[8].familyName;
     // birth.innerHTML = "Date of Birth: " + result.MRData.DriverTable.Drivers[8].dateOfBirth;
     // nacio.innerHTML = "Nacionality: " + result.MRData.DriverTable.Drivers[8].nationality;
